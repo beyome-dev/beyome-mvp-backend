962ab16428260ade1b5ec2975d432bc7145aff0a
azhar <muhammedazhar.salim@gmail.com>
2025-10-12 15:49:26 +0530

    added changes for recapp email

--- services/mailer.service.js
+++ services/mailer.service.js
@@ -19,10 +19,10 @@ const { TransactionalEmailsApi, SendSmtpEmail } = require("@getbrevo/brevo");
 // if you want to attach an image to the ejs file uncomment the attachment lines
 const getAttachments = (templateName) => {
     switch (templateName) {
-        case 'confirm-email':
+        case 'register-email':
             return [{
-                filename: 'email.png',
-                path: './public/images/email.png',
+                filename: 'demo_kit.pdf',
+                path: './public/docs/demo_kit.pdf',
                 cid: 'email_logo'
             },]
         case 'forgot-password-email':
@@ -31,67 +31,8 @@ const getAttachments = (templateName) => {
             return [];
     }
 }
-
-// const sendMail = async (to, subject, templateName, data) => {
-//     const template = fs.readFileSync(`./templates/${templateName}.ejs`, 'utf-8');
-//     const compiledTemplate = ejs.compile(template);
-//     // const attachments = getAttachments(templateName);
-
-//     const mailOptions = {
-//         from: email,
-//         to: to,
-//         subject: subject,
-//         html: compiledTemplate(data),
-//         // attachments: attachments
-//     };
-//     let info = transporter.sendMail(mailOptions);
-//     transporter.close();
-//     return info;
-// }
-
-// const transporter = nodemailer.createTransport({
-//     host: "smtp-relay.sendinblue.com",
-//     port: 587,
-//     auth: {
-//         user: config.email.brevoUser, // Your Brevo SMTP user
-//         pass: config.email.brevoPassword, // Your Brevo SMTP password
-//     },
-// });
-
-// const brevoCampaign = async (to, subject, templateName, data) => {
-//  var SibApiV3Sdk = require('sib-api-v3-sdk');
-//     //------------------
-//     // Create a campaign\
-//     // ------------------
-//     // Include the Brevo library\
-//     var defaultClient = SibApiV3Sdk.ApiClient.instance;
-//     // Instantiate the client
-//     var apiKey = defaultClient.authentications['api-key'];
-//     apiKey.apiKey = 'YOUR_API_V3_KEY';
-//     var apiInstance = new SibApiV3Sdk.EmailCampaignsApi();
-//     var emailCampaigns = new SibApiV3Sdk.CreateEmailCampaign();
-//     // Define the campaign settings\
-//     emailCampaigns.name = "Campaign sent via the API";
-//     emailCampaigns.subject = "My subject";
-//     emailCampaigns.sender = {"name": "From name", "email": "myfromemail@mycompany.com"};
-//     emailCampaigns.type = "classic";
-//     //  Content that will be sent\
-//     {
-//     htmlContent: 'Congratulations! You successfully sent this example campaign via the Brevo API.',
-//     //  Select the recipients\
-//     recipients: {listIds: [2, 7]},
-//     //  Schedule the sending in one hour\
-//     scheduledAt: '2018-01-01 00:00:01'
-// }
-//     // Make the call to the client\
-//     apiInstance.createEmailCampaign(emailCampaigns).then(function(data) {
-// console.log(API called successfully. Returned data: ' + data);
-// }, function(error) {
-// console.error(error);
-// });
-
     
-const brevoSendMail = async (toEmail, toName, subjec, htmlContent, textContent) => {
+const brevoSendMail = async (toEmail, toName, subjec, htmlContent, textContent, attachments) => {
     let emailAPI = new TransactionalEmailsApi();
     emailAPI.authentications.apiKey.apiKey = config.email.brevoApiKey
     let message = new SendSmtpEmail();
@@ -102,6 +43,8 @@ const brevoSendMail = async (toEmail, toName, subjec, htmlContent, textContent) 
     if (textContent) {
         message.textContent = textContent;
     }
+
+    message.attachment = attachments;
     message.sender = { name: "Recapp", email: email || "care@recapp.me" };
     
     // Split emails and names by comma
@@ -129,8 +72,17 @@ const sendMail = async (toEmail, toName, subject, templateName, data) => {
     //     html: compiledTemplate(data),
     // };
 
+     // Get attachments based on template
+    const attachments = getAttachments(templateName).map(attachment => ({
+        content: fs.readFileSync(attachment.path).toString('base64'),
+        name: attachment.filename,
+        // If it's an inline attachment (like images in email body)
+        disposition: attachment.cid ? "inline" : "attachment",
+        contentId: attachment.cid || undefined
+    }));
+
     try {
-        await brevoSendMail(toEmail, toName, subject, compiledTemplate(data));
+        await brevoSendMail(toEmail, toName, subject, compiledTemplate(data), attachments);
         // await transporter.sendMail(mailOptions);
         // console.log(`Email sent to ${to}`);
         // return { success: true };
